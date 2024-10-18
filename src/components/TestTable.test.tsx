74f5f0ffa0014f14dad04589866950e3402dafb1
Joseph Edusei <jedusei99@gmail.com>
2024-10-18 11:28:12 +0000

    Update table test

--- src/components/TestTable.test.tsx
+++ src/components/TestTable.test.tsx
@@ -2,12 +2,13 @@ import { describe, it } from 'vitest';
 import { render, screen, waitFor } from '@test-utils';
 import { TestTable } from './TestTable';
 import { airports } from '@/mocks/airports';
+import { debug } from 'vitest-preview';
 
 describe('Mantine datatable test', () => {
   it('should render table', async () => {
     render(<TestTable />);
     expect(screen.getAllByRole('row').length).toBeGreaterThanOrEqual(airports.length);
-    screen.logTestingPlaygroundURL();
+    debug()
     await waitFor(() => expect(screen.getByText('CFV')).toBeInTheDocument());
   });
 });
